1f445529a80b79555973dc25e573a5f956f62371
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2024-01-04 19:36:08 -0300

    core: preparation of project

---
+++ front/src/stores/contexts/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import { ExampleProvider } from './counterStore';
+
+interface AppProviderProps {
+  children: ReactNode
+}
+
+export function AppProvider({ children }: AppProviderProps) {
+  return (
+    <ExampleProvider>{children}</ExampleProvider>
+  );
+}
+
---
+++ front/src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  colors: {
+    principal: "#25D366",
+    white: "#FFFFFF",
+    black: "#010101",
+  }
+} as const
