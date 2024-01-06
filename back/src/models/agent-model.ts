13eb0c7e4558cbc256a7ed2c0d41b57cba301ff2
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2024-01-06 09:26:17 -0300

    feat: BE implemented code first in graphql

--- back/src/models/agent-model.ts
+++ back/src/models/agent-model.ts
@@ -1,18 +1,28 @@
-import { gql } from 'apollo-server'
+import { ArgsType, Field, ID, ObjectType } from "type-graphql";
+import { Agent } from "@/@types/agent";
+@ObjectType()
+export class AgentModel implements Agent {
+  @Field(type => ID)
+  id: number;
 
-export const typeDefs = gql`
-  type Agent {
-    id: ID!
-    name: String!
-    link_logo: String!
-    state_origin: String!
-    cost_kwh: Int!
-    lim_min_kwh: Int!
-    total_customers: Int!
-    evaluation_customers: Int!
-  }
+  @Field(type => String)
+  name: string;
 
-  type Query {
-    fetchAgentsByMinKwh: [Agent]
-  }
-`
+  @Field(type => String)
+  link_logo: string;
+
+  @Field(type => String)
+  state_origin: string;
+
+  @Field(type => Number)
+  cost_kwh: number;
+
+  @Field(type => Number)
+  lim_min_kwh: number;
+
+  @Field(type => Number)
+  total_customers: number;
+
+  @Field(type => Number)
+  evaluation_customers: number;
+}
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,3 +1,4 @@
+/* 
 import { app } from './app'
 import { env } from './env'
 
@@ -8,4 +9,28 @@ app
   })
   .then(() => {
     console.log(`🚀 HTTP Server running port ${env.PORT}`)
-  })
+  }) 
+*/
+import "reflect-metadata";
+
+import path from "path";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { env } from './env'
+import { FetchAgentsByMinKwhResolver } from "./resolvers/fetch-agents-by-min-kwh-resolver";
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [FetchAgentsByMinKwhResolver],
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await server.listen(env.PORT);
+  console.log(`Server is running, GraphQL available at ${url}`);
+}
+
+bootstrap();
