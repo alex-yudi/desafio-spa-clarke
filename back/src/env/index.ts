6566bd0151af8eed18f2fb9344a0c7014cccc139
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2024-01-06 12:55:25 -0300

    fix: BE correction of script to deploy 3

--- back/src/env/index.ts
+++ back/src/env/index.ts
@@ -4,35 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  JWT_SECRET: z.string(),
-  NEW_USER_LINK: z.string(),
-  RECOVERY_LINK: z.string(),
-  EMAIL_HOST: z.string(),
-  EMAIL_PORT: z.coerce.number(),
-  EMAIL_USER: z.string(),
-  EMAIL_PASS: z.string(),
-  PATH_REGISTER_OFFICE: z.string(),
-  SUBJECT_REGISTER_OFFICE: z.string(),
-  PATH_RECOVERY_PASSWORD: z.string(),
-  SUBJECT_RECOVERY_PASSWORD: z.string(),
-  PATH_REGISTER_CUSTOMER: z.string(),
-  SUBJECT_REGISTER_CUSTOMER: z.string(),
-
-  SEED_EMAIL: z.string(),
-  SEED_PASSWORD: z.string(),
-  SEED_CNPJ: z.string(),
-  SEED_NAME: z.string(),
-  SEED_ROLE: z.enum(['master']),
-
-  AWS_ACCESS_KEY_ID: z.string(),
-  AWS_SECRET_ACCESS_KEY: z.string(),
-  BUCKET_NAME: z.string(),
-  AWS_REGION: z.string(),
-
-  PLUGNOTAS_API_URL: z.string(),
-  API_KEY: z.string(),
-
-  WEBHOOK_ENDPOINT: z.string(),
+  DATABASE_URL: z.string().default('postgres://postgres:postgres@localhost:5432/boilerplate'),
 })
 
 const _env = envSchema.safeParse(process.env)
