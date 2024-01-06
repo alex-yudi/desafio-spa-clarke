a9555e288c5f30f4ab4208a3f0ac8163ad210444
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2024-01-06 09:43:17 -0300

    refact: separated code of app and server

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,34 +1,18 @@
-/* import "reflect-metadata";
+import "reflect-metadata";
 
-import path from "path";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
-import { env } from './env'
 import { FetchAgentsByMinKwhResolver } from "./resolvers/fetch-agents-by-min-kwh-resolver";
 
-async function bootstrap() {
+export async function app(port: number) {
   const schema = await buildSchema({
-    resolvers: [FetchAgentsByMinKwhResolver],
-    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+    resolvers: [FetchAgentsByMinKwhResolver]
   });
 
   const server = new ApolloServer({
     schema,
   });
 
-  const { url } = await server.listen(env.PORT);
+  const { url } = await server.listen(port);
   console.log(`Server is running, GraphQL available at ${url}`);
 }
-
-bootstrap(); 
-
-  fetchAgentsByMinKwh {
-     id,
-    name,
-    link_logo,
-    state_origin,
-    cost_kwh,
-    total_customers,
-    evaluation_customers
-  } 
-*/
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,36 +1,4 @@
-/* 
+import { env } from './env'
 import { app } from './app'
-import { env } from './env'
 
-app
-  .listen({
-    host: '0.0.0.0',
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log(`🚀 HTTP Server running port ${env.PORT}`)
-  }) 
-*/
-import "reflect-metadata";
-
-import path from "path";
-import { ApolloServer } from "apollo-server";
-import { buildSchema } from "type-graphql";
-import { env } from './env'
-import { FetchAgentsByMinKwhResolver } from "./resolvers/fetch-agents-by-min-kwh-resolver";
-
-async function bootstrap() {
-  const schema = await buildSchema({
-    resolvers: [FetchAgentsByMinKwhResolver],
-    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
-  });
-
-  const server = new ApolloServer({
-    schema,
-  });
-
-  const { url } = await server.listen(env.PORT);
-  console.log(`Server is running, GraphQL available at ${url}`);
-}
-
-bootstrap();
+app(env.PORT)
